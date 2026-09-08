073ceee7c200ecdf88ad5685ea5d7d0c04ffca16
amirhoseinmo8imi-hash <amirhoseinmo8imi@gmail.com>
2026-09-08 14:13:36 +0330

    Add checkout preflight validation tests

---
+++ tests/test_checkout_preflight.py
@@ -0,0 +1,106 @@
+from flask import Flask
+
+from app import BASE_DIR, db
+from checkout_preflight import apply_checkout_preflight
+from security_hardening import apply_security, csrf_token
+
+
+def make_app():
+    app = Flask(__name__)
+    app.config["TESTING"] = True
+    app.config["SECRET_KEY"] = "checkout-preflight-test-secret"
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + str(BASE_DIR / "kharidino.db")
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.init_app(app)
+    with app.app_context():
+        db.create_all()
+        apply_security(app)
+        apply_checkout_preflight(app)
+    return app
+
+
+def register_checkout(app):
+    @app.get("/token")
+    def token():
+        return csrf_token()
+
+    @app.post("/checkout")
+    def checkout():
+        return "created"
+
+
+def authenticated_client(app):
+    client = app.test_client()
+    token = client.get("/token").get_data(as_text=True)
+    with client.session_transaction() as sess:
+        sess["user_id"] = 424243
+        sess["cart"] = {"991002": 1}
+    return client, token
+
+
+def valid_data(token):
+    return {
+        "customer_name": "Test User",
+        "phone": "09000000002",
+        "address": "Test Address 123",
+        "note": "",
+        "csrf_token": token,
+    }
+
+
+def test_checkout_preflight_accepts_valid_customer_and_cart():
+    app = make_app()
+    register_checkout(app)
+    client, token = authenticated_client(app)
+    response = client.post("/checkout", data=valid_data(token), headers={"Origin": "http://localhost"})
+    assert response.status_code == 200
+
+
+def test_checkout_preflight_rejects_missing_customer_field():
+    app = make_app()
+    register_checkout(app)
+    client, token = authenticated_client(app)
+    data = valid_data(token)
+    data["address"] = ""
+    response = client.post("/checkout", data=data, headers={"Origin": "http://localhost"})
+    assert response.status_code == 400
+
+
+def test_checkout_preflight_rejects_invalid_phone():
+    app = make_app()
+    register_checkout(app)
+    client, token = authenticated_client(app)
+    data = valid_data(token)
+    data["phone"] = "not-a-phone"
+    response = client.post("/checkout", data=data, headers={"Origin": "http://localhost"})
+    assert response.status_code == 400
+
+
+def test_checkout_preflight_rejects_empty_cart():
+    app = make_app()
+    register_checkout(app)
+    client, token = authenticated_client(app)
+    with client.session_transaction() as sess:
+        sess["cart"] = {}
+    response = client.post("/checkout", data=valid_data(token), headers={"Origin": "http://localhost"})
+    assert response.status_code == 400
+
+
+def test_checkout_preflight_rejects_malformed_cart_quantity():
+    app = make_app()
+    register_checkout(app)
+    client, token = authenticated_client(app)
+    with client.session_transaction() as sess:
+        sess["cart"] = {"991002": "not-an-int"}
+    response = client.post("/checkout", data=valid_data(token), headers={"Origin": "http://localhost"})
+    assert response.status_code == 400
+
+
+def test_checkout_preflight_rejects_oversized_note():
+    app = make_app()
+    register_checkout(app)
+    client, token = authenticated_client(app)
+    data = valid_data(token)
+    data["note"] = "x" * 2001
+    response = client.post("/checkout", data=data, headers={"Origin": "http://localhost"})
+    assert response.status_code == 400

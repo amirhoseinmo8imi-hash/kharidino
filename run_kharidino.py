e78af872e4ebb91e9bb520786eafce5093642e90
amirhoseinmo8imi-hash <amirhoseinmo8imi@gmail.com>
2026-09-10 12:43:00 +0330

    feat: wire wallet and coupon into checkout lifecycle

--- run_kharidino.py
+++ run_kharidino.py
@@ -55,6 +55,7 @@ from clawback_reconciliation import apply_clawback_reconciliation
 from financial_reconciliation import apply_financial_reconciliation
 from marketplace_ultimate import apply_marketplace_ultimate
 from marketplace_hardening import apply_marketplace_hardening
+from checkout_marketplace_adjustments import apply_checkout_marketplace_adjustments
 import commerce_runtime
 import profile_extensions
 import merchant_marketplace
@@ -78,7 +79,7 @@ with app.app_context():
     apply_catalog_ux_api(app, db, Product, Category, Offer, Store); apply_checkout_preflight(app)
     apply_checkout_idempotency(app, db); apply_inventory_security(app); apply_inventory_atomicity(app)
     apply_order_state_machine(app); apply_commerce_extensions(app); apply_catalog_extensions(app)
-    apply_marketplace_ultimate(app); apply_marketplace_hardening(app)
+    apply_marketplace_ultimate(app); apply_marketplace_hardening(app); apply_checkout_marketplace_adjustments(app)
     apply_payment(app, db, __import__("app").Order, User)
     from merchant_marketplace_v2 import SellerLedger, SellerOrder
     from accounting import SellerSettlement, SellerSettlementAllocation, ensure_settlement_allocation_guard

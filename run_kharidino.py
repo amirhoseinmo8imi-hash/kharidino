4fe1018ed9a7ca4224b41d592373a113d14cbc5e
amirhoseinmo8imi-hash <amirhoseinmo8imi@gmail.com>
2026-09-10 12:51:18 +0330

    Activate return refund bridge after refund hardening

--- run_kharidino.py
+++ run_kharidino.py
@@ -51,6 +51,7 @@ from order_state_machine import apply_order_state_machine
 from payment import apply_payment
 from button_flow_hardening import apply_button_flow_hardening
 from refund_settlement_hardening import apply_refund_settlement_hardening
+from return_refund_bridge import apply_return_refund_bridge
 from clawback_reconciliation import apply_clawback_reconciliation
 from financial_reconciliation import apply_financial_reconciliation
 from marketplace_ultimate import apply_marketplace_ultimate
@@ -88,6 +89,7 @@ with app.app_context():
     PaymentTransaction = app.extensions["kharidino_payment_transaction"]
     apply_financial_accounting(app, db, __import__("app").Order, SellerLedger, SellerSettlement)
     apply_refund_settlement_hardening(app, db, __import__("app").Order, PaymentTransaction, SellerOrder, SellerLedger, SellerSettlement, SellerSettlementAllocation)
+    apply_return_refund_bridge(app, db, __import__("app").Order, PaymentTransaction)
     apply_clawback_reconciliation(app, db, Store)
     apply_button_flow_hardening(app, db, Store, User)
     db.create_all()
